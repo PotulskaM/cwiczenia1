05df76593502ca4ae39fde3650fb00c7b0e2c1d6
Małgorzata Potulska <m.pootulska@gmail.com>
2019-03-02 12:49:28 +0100

    inicjacja dzień drugi

---
+++ src/main/java/pl/teb/edukacja/cwiczenia1/Cwiczenia1Application.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.cwiczenia1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cwiczenia1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cwiczenia1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/teb/edukacja/cwiczenia1/Cwiczenia1ApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.teb.edukacja.cwiczenia1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cwiczenia1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
